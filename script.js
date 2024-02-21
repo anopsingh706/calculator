51d6741c3fb05524dc8ad01c7327d53c0c8c6016
Anop singh <anops706@gmail.com>
2024-02-21 10:27:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+        function printvalue1(button){
+            form.screen.value+='1';
+
+        } 
+        function printvalue2(button){
+            form.screen.value+='2';
+        }
+        function printvalue3(button){
+            form.screen.value+='3';
+        }
+        function printvalueplus(button){
+            form.screen.value+='+';
+        }
+        function printvalue4(button){
+            form.screen.value+='4';
+        }
+        function printvalue5(button){
+            form.screen.value+='5';
+        } 
+        function printvalue6(button){
+            form.screen.value+='6';
+        }
+        function printvalue7(button){
+            form.screen.value+='7';
+        }
+        function printvaluem(button){
+            form.screen.value+='-';
+        }
+        function printvalue8(button){
+            form.screen.value+='8';
+        }
+        function printvalue9(button){
+            form.screen.value+='9';
+        } 
+        function printvalue0(button){
+            form.screen.value+='0';
+        }
+        function printvaluea(button){
+            form.screen.value+='*';
+        }
+        function printvalue_dot(button){
+            form.screen.value+='.';
+        }
+        function printvalued(button){
+            form.screen.value+='/';
+        }
+        function printvalue_e(button){
+            form.screen.value= eval(form.screen.value);
+        } 
+        function printvaluec(button){
+         form.screen.value="";
+        }
+        
+    
